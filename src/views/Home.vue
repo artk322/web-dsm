92f177fe92867a27a9f7e80ba9fd0d80e5be7549
arthur <arthur.kentbayev@gmail.com>
2021-12-18 22:12:28 +0600

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row>
+    <Restaurant v-for="item in 4" :key="item" />
+  </v-row>
+</template>
+
+<script>
+import Restaurant from '../components/Restaurant.vue';
+
+export default {
+  name: 'Home',
+
+  components: {
+    Restaurant,
+  },
+};
+</script>
